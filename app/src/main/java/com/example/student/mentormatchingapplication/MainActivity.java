14d44e8c4771909ac9582aa9e65eb9a613e1859b
student <eternalasset@ajou.ac.kr>
2018-04-10 19:33:30 +0900

    dddd

---
+++ app/src/main/java/com/example/student/mentormatchingapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.student.mentormatchingapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hello World
+        //Hello World2
+        //Hello World3
+        //수정ㅁ니;아럼ㅈ다ddddd
+    }
+}
